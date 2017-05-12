1717c8bf3e89ca4204187505551a598b020886b6
devbywater <egaraudy@gmail.com>
2017-05-12 11:06:54 -0600

    modify style classes

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ var utils = require('../utils')
 router.post('/:action', function(req, res, next){
     var action = req.params.action
     if(action === 'send'){
-        var list = req.body.recipients.split(',')
+        var list = req.body.recipients
 
         utils.Email.sendEmails(list, req.body, function(){
             res.json({
