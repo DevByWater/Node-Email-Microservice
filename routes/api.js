0debe64526cd426913e285abc24ece7330c1b865
devbywater <egaraudy@gmail.com>
2017-05-11 20:38:46 -0600

    send email through sendgrid

--- routes/api.js
+++ routes/api.js
@@ -37,4 +37,39 @@ router.get('/:action', function(req, res, next){
    
 })
 
+router.post('/:action', function(req, res, next){
+    var action = req.params.action
+    if(action === 'send'){
+        //send emails
+        var from_email = new helper.Email('egaraudy@gmail.com');
+        var to_email = new helper.Email(req.body.recipient);
+        var subject = req.body.subject;
+        var content = new helper.Content('text/html', req.body.content);
+        var mail = new helper.Mail(from_email, subject, to_email, content);
+
+        var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+        var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON(),
+        });
+
+        sg.API(request, function(error, response) {
+            if(error){
+                res.json({
+                    confirmation: 'fail',
+                    message: error
+                })
+                return
+            }
+             res.json({
+                confirmation: 'success',
+                response: response
+            })
+        });
+        
+    }
+   
+})
+
 module.exports = router
